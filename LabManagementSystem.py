c151e407ac407e15ae6e149358eddc46b6ee46ef
DepartureZSH <91520014+DepartureZSH@users.noreply.github.com>
2023-06-26 18:29:57 +0800

    Delete LabManagementSystem.py

--- LabManagementSystem.py
+++
@@ -1,9 +0,0 @@
-import sys
-from Main import Demo
-from PyQt5.QtWidgets import QApplication
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    demo = Demo()
-    demo.show()
-    sys.exit(app.exec_())
